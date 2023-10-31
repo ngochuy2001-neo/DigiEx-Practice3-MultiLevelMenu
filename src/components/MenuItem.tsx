330d111b5f4203e496fc7bb01df3b0cb687159de
ngochuy2001-neo <ngochuy11010@gmail.com>
2023-10-31 13:42:13 +0700

    update UI color

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -6,7 +6,7 @@ const MenuItem = ({itemTitle, itemBranch}: IMenuItem) =>{
 
   const [isHover, setIsHover]= useState<boolean>(false);
   return (
-    <div className="w-[150px] flex justify-center items-center h-[50px] border-2 border-gray-500" onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>
+    <div className="w-[150px] flex justify-center items-center h-[50px] border-black border-2" onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>
       {itemTitle}
       {Object.keys(itemBranch).length !=0 && isHover? <MenuItemList itemList={itemBranch} /> :""}
     </div>
--- src/components/MenuItemsList.tsx
+++ src/components/MenuItemsList.tsx
@@ -4,7 +4,7 @@ import MenuItem from "./MenuItem";
 const MenuItemList = ({itemList, firstFlag}: IMenuItemsList) => {
   const items = Object.keys(itemList);
   return (
-    <div className="absolute" style={firstFlag? {} : {transform: "translateX(100%)"}}>
+    <div className="absolute bg-white" style={firstFlag? {} : {transform: "translateX(100%)"}}>
       {items.map((value, index) => (
         <MenuItem itemTitle={value} itemBranch={itemList[value]} key={index}/>
       ))}
